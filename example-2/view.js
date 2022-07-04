b8164dd85785b273d1abad457f1ef559ed8ede5a
Aisha-Yusuff <a.yusuff99@outlook.com>
2022-07-04 20:30:35 +0100

    Created addParagraph method and passing test

--- example-2/view.js
+++ example-2/view.js
@@ -1,8 +1,15 @@
 class View {
   constructor() {
-    this.mainContainerEl = document.querySelector('#main-container');
+    this.mainContainerEl = document.querySelector("#main-container");
 
     console.log(this.mainContainerEl);
+  }
+
+  addParagraph() {
+    let paragraph = document.createElement("p");
+    paragraph.innerText =
+      "This paragraph has been dynamically added by JavaScript!";
+    this.mainContainerEl.append(paragraph);
   }
 }
 
